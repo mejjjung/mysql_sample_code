d10114c691d5e504a2e221cc093b786a9603bae1
mejjjung <zalwjdz29@naver.com>
2023-03-06 16:39:55 +0900

    INSERT 구문과 제약 사항 최종

--- INSERT 구문과 제약 사항.sql
+++ INSERT 구문과 제약 사항.sql
@@ -100,4 +100,136 @@ INSERT INTO product(id, name, description)
 VALUES (2, '바나나','노란 바나나');
 
 INSERT INTO product(id, name, description)
-VALUES (3, '포도','동글동글 포도')
+VALUES (3, '포도','동글동글 포도');
+
+desc student;
+
+-- 테이블 제약사항에 NULL 허용 의미와 NOT NULL 허용 의미
+
+-- 이미 생성된 테이블 제약 사항 변경하기
+ALTER TABLE student MODIFY grade INT NULL;
+
+desc student;
+
+INSERT INTO student(student_id, name, major)
+VALUES(1, '홍길동','검술학과');
+
+-- grade 제약을 NOT NULL 변경 해보기
+-- 테이블 값이 하나 들어가 있는 상태
+ALTER TABLE student MODIFY grade INT NOT NULL;
+
+SELECT * FROM student;
+-- 하나의 행을 삭제하는 쿼리 - WHERE절 (조건절) 
+DELETE FROM student WHERE student_id = 1;
+
+-- major 컬럼 제약을 NOT NULL 변경해 주세요
+ALTER TABLE student MODIFY major VARCHAR(50) NOT NULL;
+
+-- 컬럼 선언 부분을 생략도 가능하다. 단 전부 값을 넣을려고 하는 상황일 때 
+INSERT INTO student VALUES(2, '홍길동2', 3,'검술학과');
+
+-- insert 구문 컬럼명 선언 방식
+-- 1. 전부 작성하는 방법alter
+-- 2. 제약 사항이 null인 열이 있다면 넣고 싶은 열만 지정해서 구문을 작성할 수 있다. 
+-- 3. 전부 데이터를 넣는 상황이라면 컬럼 선언 부분을 생략 가능하다.
+
+-- NOT NULL과 디폴트값 사용
+
+DROP TABLE student;
+
+CREATE TABLE student(
+	student_id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    grade INT NOT NULL,
+    major VARCHAR(50) DEFAULT '미정' NOT NULL 
+);
+
+DESC student;
+
+-- 디폴트 제약을 사용했을 때 값을 넣은 방법 1
+INSERT INTO student(student_id, name, grade, major)
+VALUES
+	(1, 'John',3,'경영학과'),
+    (2, 'Jane',2,'경영학과'),
+    (3, 'Mike',1,'DEFAULT'),
+    (4, 'David',2,'DEFAULT');
+    
+SELECT * FROM student;
+-- 디폴트 제약을 사용했을 때 값을 넣은 방법 2
+
+INSERT INTO student(student_id, name, grade)
+VALUES
+	(5,'홍길동1',2),
+    (6,'홍길동2',3),
+    (7,'홍길동3',1),
+    (8,'홍길동4',3);
+ 
+ -- professor 컬럼 제약 사항을 default로 추가 하고 값을 빈 문자열로 만들어 주세요 
+ -- 빈 문자열의 의미는 ''
+ CREATE TABLE subject (
+  subject_id INT PRIMARY KEY,
+  subject_name VARCHAR(50) NOT NULL,
+  credit INT NOT NULL,
+  department VARCHAR(50) NOT NULL,
+  professor VARCHAR(50) NOT NULL
+);
+SELECT * FROM subject;
+
+-- 테이블이 생성 되어 있을 때 제약 변경 	default 구문 추가 
+ALTER TABLE subject MODIFY professor VARCHAR(50) NOT NULL DEFAULT "";
+
+SHOW CREATE TABLE `order`;
+
+DROP TABLE `order`;
+
+CREATE TABLE `order` (
+	id INT PRIMARY KEY,
+   customer_order VARCHAR(50) NOT NULL,
+   product_name VARCHAR(20) NOT NULL,
+   quantity INT NOT NULL,
+   order_date DATE NOT NULL
+);
+
+-- 데이터를 입력하는 쿼리 구문을 작성해 주세요 
+INSERT INTO `order` (id, customer_order, product_name, quantity, order_date) 
+VALUES (1, '빠른 배송 바람', '신라면', 2, '2022-02-25');
+
+SELECT * FROM `order`;
+
+INSERT INTO `order` (id, customer_order, product_name, quantity, order_date) 
+VALUES (2, '빠른 배송 바람', '신라면', 2, 20220225);
+
+-- DATE 타입에 값을 넣을 때 작은따옴표나 큰따옴표를 사용해도 되고 
+-- 정수값을 입력하여도 된다. 단 날짜 형식에 맞는 값이어야 한다. 
+-- 1년은 12개월 인데 13이라는 값을 넣으면 알아서 오류를 일으켜 준다. 
+INSERT INTO `order` (id, customer_order, product_name, quantity, order_date) 
+VALUES (2, '빠른 배송 바람', '신라면', 2, 20222225);
+
+INSERT INTO `order` (id, customer_order, product_name, quantity, order_date) 
+VALUES (2, '빠른 배송 바람', '신라면', 2, '20222225');
+
+
+
+DROP TABLE customer;
+
+-- YYYY-MM-DD : mysql 기본 DATE의 데이터 타입 기본 포멧 
+-- YYYY/MM/DD
+-- YYYYMMDD
+
+-- AUTO_INCREMENT 사용하기
+-- 고객 테이블 생성
+
+CREATE TABLE customer(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50),
+    address VARCHAR(100) NOT NULL
+); 
+
+INSERT INTO customer(name, email, address)
+VALUES
+	('김철수','A@naver.com','서울시 강남구 역삼동'),
+    ('박영희','B@naver.com','서울시 강동구 천호동'),
+    ('김철수','A@naver.com','서울시 강남구 역삼동');
+    
+SELECT * FROM customer;
